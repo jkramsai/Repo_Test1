c28ed63b32498ff629e74b216f16352710ae2034
pbyri <pavansemail@gmail.com>
2017-02-20 08:15:44 -0800

    added cpp project

---
+++ addcpp/build/CMakeFiles/addcpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/addcpp.dir/main.cpp.o"
+  "addcpp.pdb"
+  "addcpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/addcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
